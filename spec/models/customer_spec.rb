0d66eaa43f03089dc84b9fd011f493ca4e525554
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-18 08:54:11 -0300

    chore: factorybot aliases and params override examples

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe Customer, type: :model do
     expect(customer.full_name).to start_with('Sr. ')
   end
 
+  it '#full name (override random values)' do
+    customer = create(:customer, name: 'Felipe', email: 'felipe@gmail.com')
+    expect(customer.name).to eq('Felipe')
+    expect(customer.full_name).to eq('Sr. Felipe')
+    expect(customer.email).to eq('felipe@gmail.com')
+  end
+
+  it '#full_name (with factory aliases)' do
+    client = create(:client)
+    expect(client.full_name).to start_with('Sr. ')
+  end
+
   it { expect { create(:customer) }.to change { Customer.all.size }.by(1) }
 end
