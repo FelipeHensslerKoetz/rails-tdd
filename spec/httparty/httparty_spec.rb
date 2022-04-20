90436e07c22194302dfbf78b0bedbcd96afbdf7d
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-19 22:05:11 -0300

    chore: new episodes record type example

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 describe 'HTTParty' do
-  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
-    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+  # it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body] } do
+  # :record => :new_episodes
+  it 'content-type', vcr: { cassette_name: 'jsonplaceholder/posts', record: :new_episodes } do
+    response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/3')
     content_type = response.headers['content-type']
     expect(content_type).to eq('application/json; charset=utf-8')
   end
