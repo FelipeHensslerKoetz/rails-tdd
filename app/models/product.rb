dd8432e36e6210247bf4e84eb8c1d07bc98d0cb7
Felipe Henssler Koetz <felipehenssler@gmail.com>
2022-04-21 00:56:22 -0300

    chore: add product validations and specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :description, :price, :category, presence: true
+
+  def full_description
+    "#{description} - #{price}"
+  end
 end
